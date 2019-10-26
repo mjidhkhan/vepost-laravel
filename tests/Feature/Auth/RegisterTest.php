305d8b3ad3b9a503fb7d403e72cc6830d46221cc
MH Khan <majidhkhan@outlook.com>
2019-10-26 18:36:18 +0100

    WIP Register Test

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -8,5 +8,32 @@ use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
-    
+
+    use RefreshDatabase;
+
+    protected function successfulRegistrationRoute()
+    {
+        return route('home');
+    }
+    protected function registerGetRoute()
+    {
+        return route('register');
+    }
+    protected function registerPostRoute()
+    {
+        return route('register');
+    }
+    protected function guestMiddlewareRoute()
+    {
+        return route('home');
+    }
+
+    /** @test */
+    public function user_can_view_a_registration_form()
+    {
+        $response = $this->get($this->registerGetRoute());
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.register');
+    }
+
 }

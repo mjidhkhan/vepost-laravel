d4ab01b7e1c7cc1afbd83ee36d2fb7b1be47b080
MH Khan <majidhkhan@outlook.com>
2019-10-26 18:36:05 +0100

    WIP email cerification test

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -8,5 +8,49 @@ use Tests\TestCase;
 
 class EmailVerificationTest extends TestCase
 {
-    
+        use RefreshDatabase;
+
+    protected $verificationVerifyRouteName = 'verification.verify';
+
+    protected function successfulVerificationRoute()
+    {
+        return route('home');
+    }
+
+    protected function verificationNoticeRoute()
+    {
+        return route('verification.notice');
+    }
+
+    protected function validVerificationVerifyRoute($id)
+    {
+        return URL::signedRoute($this->verificationVerifyRouteName, ['id' => $id]);
+    }
+
+    protected function invalidVerificationRouteName($id)
+    {
+        return route($this->verificationVerifyRouteName, ['id' => $id]). '?signature=invalid-signature';
+    }
+
+    protected function verificationResendRoute()
+    {
+        return route('verification.resend');
+    }
+
+    protected function loginRoute()
+    {
+        return route('login');
+    }
+
+    /** @test */
+    public function guest_cannot_see_the_verification_notice()
+    {
+        //Given
+
+        //When
+        $response = $this->get($this->verificationNoticeRoute());
+        $response->assertRedirect($this->loginRoute());
+
+        //Then
+    }
 }

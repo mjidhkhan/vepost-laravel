cf7b4c5f763864773596956177f2b774840b2709
MH Khan <majidhkhan@outlook.com>
2019-10-26 17:47:32 +0100

    created register verification test

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    
+}

ad14dd8a853c2835176c064dc09e1e3849a8ee80
G3TLeighton <gwl@g3t.uk.com>
2019-10-28 15:19:22 +0000

    add new migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username');
+            $table->string('vepost_code');
+            $table->string('vepost_address');
+            $table->string('display_name');
+            $table->string('control_string')->default('00000000000000000000')->nullable();
+            $table->string('balance')->nullable();
+            $table->string('vepost_counter')->nullable();
+            $table->string('status')->default(0)->nullable();
+            $table->string('free_send_left')->nullable();
+            $table->string('country_code');
+            $table->string('phone');
+            $table->string('vep_code');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

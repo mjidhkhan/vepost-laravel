379f03cf05f96bcbefb2d89d51f90fa1fe6b7194
G3TLeighton <gwl@g3t.uk.com>
2019-11-07 16:46:47 +0000

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,80 +2,72 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="mb-4">
-                    <form method="POST" action="{{ route('register') }}">
+
+<!-- ----------------------- -->
+
+
+<div class="mx-auto flex flex-col   items-center">
+               
+
+                
+                    <form method="POST" action="{{ route('register') }}" class="w-4/12  bg-white shadow-md px-8 pt-6 pb-8 mb-4 items-center">
                         @csrf
-
-                        <div class="form-group row">
-                           
-                            <div class="col-md-12 col-sm-12 scrollpoint  alert alert-info alert-bottom" id="signuptop">
-                                    <label for="example-text-input" class="cblock text-gray-700 text-sm font-bold mb-2">{{ __('VePost Box No') }}</label> 
-                                <div class="col-md-6">
-                                        <input id="vep_code" 
-                                        type="text" 
-                                        class="appearance-none border  w-full py-2 px-3 text-gray-400 leading-tight" 
-                                        name="vep_code" 
-                                        value="233"  
-                                        style="width:54px" readonly>
-                                        <input id="country_code" type="text" class="form-control pull-right @error('country_code') is-invalid @enderror" name="country_code" value="{{ old('country_code') }}"  style="width: 36px;padding-left: 37px;" autofocus>
-                                </div>
-
+                         <div class="card-header text-4xl">{{ __('Register') }}</div>
+                             <label for="example-text-input" class="cblock text-gray-700 text-sm font-bold mb-2">{{ __('VePost Box No') }}</label> 
+                         <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3  bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
                                 <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-        VePost Code
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Coutry Code
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <input id="vepost_address" 
-                                    type="text" 
-                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('vepost_address') is-invalid @enderror" 
-                                    name="vepost_address" value="{{ old('vepost_address') }}"  
-                                    autocomplete="vepost_address" 
-                                    autofocus>
-        </div>
-      </div>
-    </div>
-                                   
-                                <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Numner') }}</label>
-                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="name" autofocus>
+                                    <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                        VePost Code
+                                    </label>
+                                    <input class="appearance-none block w-16  text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:none" 
+                                            name="vep_code"id="grid-city" type="text" value="233" readonly>
+                                    </div>
+                                    <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                                    Country
+                                    </label>
+                                    <div class="relative">
+                                        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('country_code') is-invalid @enderror" name="country_code" value="{{ old('country_code') }}">
+                                        <option>+44</option>
+                                        <option>+1</option>
+                                        <option>+92</option>
+                                        </select>
+                                        
+                                    </div>
+                                    </div>
+                                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 " for="grid-zip">
+                                        {{ __('Phone Numner') }}
+                                    </label>
+                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" id="grid-zip" type="text" placeholder="90210">
+                                    </div>
+                                </div>
 
                                 @error('phone')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                                </div>
-                            
+
                         </div>
                         
+              
 
                         
                         
                         
                         
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
+                            
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  autocomplete="name" autofocus>
+                                 <label for="username" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Full name') }}</label>
+                                <input id="name" type="text" 
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  @error('name') is-invalid @enderror" 
+                                name="name" 
+                                value="{{ old('name') }}"  
+                                autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -85,10 +77,15 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('User Name') }}</label>
+                            
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}"   >
+                                 <label for="username" class="block text-gray-700 text-sm font-bold mb-2">{{ __('User Name') }}</label>
+                                <input id="username" 
+                                        type="text" 
+                                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  @error('username') is-invalid @enderror" 
+                                        name="username" 
+                                        value="{{ old('username') }}">
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -98,11 +95,10 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="displayname" class="col-md-4 col-form-label text-md-right">{{ __('Display Name') }}</label>
-
+                            
+                             <label for="display_name" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Display Name') }}</label>
                             <div class="col-md-6">
-                                <input id="display_name" type="text" class="form-control @error('display_name') is-invalid @enderror" name="display_name" value="{{ old('display_name') }}"  >
-
+                                <input id="display_name" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('display_name') is-invalid @enderror" name="display_name" value="{{ old('display_name') }}"  >
                                 @error('displayname')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -112,10 +108,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
+                           
+                             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('E-Mail Address') }}</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
+                                <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -126,10 +122,11 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                             <label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Password') }}</label>
+                           
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
+                                <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -140,28 +137,24 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
+                             <label for="password-confirm" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Confirm Password') }}</label>
+                            
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password_confirmation"  autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        
+                            <div class="col-md-6 offset-md-4 pt-5">
+                                <button type="submit" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                                     {{ __('Register') }}
                                 </button>
                             </div>
-                        </div>
+                        
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
+           </div>
+          
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet"> 
 </head>
 <body>
     <div id="app" class=" bg-gray-200 h-screen">
